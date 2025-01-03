2ae8d88193b4c9abda758e6215b4e8c331a98f8e
TaiwoForbes <soyemihissa2000@gmail.com>
2025-01-02 18:37:43 -0800

    setting up toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router'
 import Login from './pages/Login/Login'
 import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path='/' element = {<Login/>}/>
         <Route path='/chat' element = {<Chat/>}/>
