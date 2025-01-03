2a5b6d6c8ceaf1a03c7095cacab77416ea4963dc
TaiwoForbes <soyemihissa2000@gmail.com>
2025-01-02 17:42:20 -0800

    setting up firebase authentication

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkIAqWVyHnl6Jofd6-70y5Eq9CJRcv8Qk",
+  authDomain: "chat-app-203a2.firebaseapp.com",
+  projectId: "chat-app-203a2",
+  storageBucket: "chat-app-203a2.firebasestorage.app",
+  messagingSenderId: "204983463683",
+  appId: "1:204983463683:web:12749d57e49900d4bb0336"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
