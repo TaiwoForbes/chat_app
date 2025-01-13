8f947920dc2d668f9a6a9770b03c284f3694dca8
TaiwoForbes <soyemihissa2000@gmail.com>
2025-01-13 07:10:48 +0100

    pushing my recent changes

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -7,32 +7,28 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../configs/firebase";
 import { AppContext } from "../../context/AppContext";
 const LeftSideBar = () => {
-  const navigate = useNavigate()
-  const {userData} = useContext(AppContext)
-  const [user,setUser] = useState(null)
-  const [showSearch,setShowSearch] = useState(false)
+  const navigate = useNavigate();
+  const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
-  const InputHandler = async (e)=>{
+  const InputHandler = async (e) => {
     try {
-      const input = e.target.value
+      const input = e.target.value;
       if (input) {
-        setShowSearch(true)
-        const userRef = collection(db,'users')
-        const q = query(userRef,where('username' ,'==',input.toLowerCase()))
-        const querySnap = await getDocs(q)
+        setShowSearch(true);
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("username", "==", input.toLowerCase()));
+        const querySnap = await getDocs(q);
         if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
-            setUser(querySnap.docs[0].data())        
-        }
-        else{
-          setUser(null)
-        }
-      }else{
-        setShowSearch(false)
+          setUser(querySnap.docs[0].data());
+          console.log(user);
+        } 
+      } else {
+        setShowSearch(false);
       }
-    } catch (error) {
-      
-    }
-  }
+    } catch (error) {}
+  };
 
   return (
     <div className="ls">
@@ -42,7 +38,13 @@ const LeftSideBar = () => {
           <div className="menu">
             <img src={assets.menu_icon} alt="" />
             <div className="sub-menu">
-              <p onClick={()=>{navigate('/profile')}}>Edit Profile</p>
+              <p
+                onClick={() => {
+                  navigate("/profile");
+                }}
+              >
+                Edit Profile
+              </p>
               <hr />
               <p>Logout</p>
             </div>
@@ -50,21 +52,32 @@ const LeftSideBar = () => {
         </div>
         <div className="ls-search">
           <img src={assets.search_icon} alt="" />
-          <input onChange={InputHandler} type="text" placeholder="Search here..," />
+          <input
+            onChange={InputHandler}
+            type="text"
+            placeholder="Search here..,"
+          />
         </div>
       </div>
       <div className="ls-list">
-        {Array(12)
-          .fill("")
-          .map((item, index) => (
-            <div key={index} className="friends">
-              <img src={assets.profile_img} alt="" />
-              <div>
-                <p>Eichard Sanford</p>
-                <span>Hello,How are you?</span>
+        {showSearch && userData ? (
+          <div className="friends add-user">
+            <img src={assets.profile_img} alt="" />
+            <p>{user.name}</p>
+          </div>
+        ) : (
+          Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                  <p>Eichard Sanford</p>
+                  <span>Hello,How are you?</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+        )}
       </div>
     </div>
   );
